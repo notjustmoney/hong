9202784a8201310bfa0a419176f2e8fcfc968dce
notjustmoney <chajy97@gmail.com>
2020-05-23 22:32:50 +0900

    add post route that include post, comment management API

--- server2/src/routes/index.js
+++ server2/src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const userRoute = require('./user.route');
 const authRoute = require('./auth.route');
+const postRoute = require('./post.route');
 
 const router = express.Router();
 
 router.use('/users', userRoute);
 router.use('/auth', authRoute);
+router.use('/post', postRoute);
 
 module.exports = router;
