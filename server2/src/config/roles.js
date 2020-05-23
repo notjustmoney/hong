a26ca45e2325c70407966b86c7a6cee2bb2cece6
notjustmoney <chajy97@gmail.com>
2020-05-23 22:25:41 +0900

    add user roles

--- server2/src/config/roles.js
+++ server2/src/config/roles.js
@@ -1,9 +1,9 @@
 const roles = ['user', 'productor', 'admin'];
 
 const roleRights = new Map();
-roleRights.set(roles[0], []);
-roleRights.set(roles[1], []);
-roleRights.set(roles[2], ['getUsers', 'manageUsers']);
+roleRights.set(roles[0], ['createComment', 'manageComment']);
+roleRights.set(roles[1], ['createComment', 'manageComment', 'createPost', 'managePost']);
+roleRights.set(roles[2], ['createComment', 'manageComment', 'createPost', 'managePost', 'getUsers', 'manageUsers']);
 
 module.exports = {
   roles,
