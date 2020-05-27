87bb4fc7c9d96fd43c9887717d2484c5a3d21cd8
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-05-27 19:25:46 +0900

    edit client

--- client/src/routes/QnA.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const QnA = () => {
-  return <div>Hello Qna</div>;
-};
-
-export default QnA;
--- client2/src/components/Router.js
+++ client2/src/components/Router.js
@@ -1,20 +1,17 @@
 import React from "react";
-import {
-  HashRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "../routes/Main";
 import Header from "./Header";
 import QnA from "../routes/QnA";
+import RegisterForm from "./RegistForm";
 
 export default () => (
   <Router>
     <Header />
     <Switch>
       <Route exact path="/" component={Main} />
-      <Route exact path="/qna" component={QnA} />
+      <Route exact path="/about" component={QnA} />
+      <Route exact path="/register" component={RegisterForm} />
     </Switch>
   </Router>
 );
