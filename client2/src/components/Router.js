ef94b3a74157884656056f35642e1564acfc7e04
starpark <62285975+starpark@users.noreply.github.com>
2020-06-12 19:44:26 +0900

    change hashrotuer into browserrouter

--- client2/src/components/Router.js
+++ client2/src/components/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "../routes/Main";
 import QnA from "../routes/QnA";
 import RegisterForm from "./RegistForm";
