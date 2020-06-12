be87fb6676a97e93edbd4f693d1ee977d9201d6a
notjustmoney <chajy97@gmail.com>
2020-06-12 23:14:31 +0900

    modify routes structure

--- server2/src/routes/search.route.js
+++ server2/src/routes/search.route.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-await-in-loop */
 const express = require('express');
 const validate = require('../middlewares/validate');
@@ -8,33 +9,49 @@ const Post = require('../models/post.model');
 
 const router = express.Router();
 
+const getPostsByTag = async (tags) => {
+  const posts = [];
+  for (const elem of tags.posts) {
+    const post = await Post.findById(elem)
+      .populate({
+        path: 'comments',
+        select: 'contents writer createdAt',
+        populate: {
+          path: 'writer',
+          select: 'profile',
+        },
+      })
+      .populate({
+        path: 'tags',
+        select: 'hashtag',
+      })
+      .populate({
+        path: 'writer',
+        select: 'profile',
+      });
+    posts.push(post);
+  }
+  return posts;
+};
+
 router.get(
-  '/:tagId',
+  '/tag',
+  validate(searchValidation.searchByTagName),
+  catchAsync(async (req, res) => {
+    const { tagName } = req.body;
+    const tags = await Hashtag.findOne({ hashtag: tagName });
+    const posts = await getPostsByTag(tags);
+    const response = posts;
+    res.send(response);
+  })
+);
+
+router.get(
+  '/tag/:tagId',
   validate(searchValidation.searchByTagId),
   catchAsync(async (req, res) => {
     const tags = await Hashtag.findById(req.params.tagId);
-    const posts = [];
-    // eslint-disable-next-line no-restricted-syntax
-    for (const elem of tags.posts) {
-      const post = await Post.findById(elem)
-        .populate({
-          path: 'comments',
-          select: 'contents writer createdAt',
-          populate: {
-            path: 'writer',
-            select: 'profile',
-          },
-        })
-        .populate({
-          path: 'tags',
-          select: 'hashtag',
-        })
-        .populate({
-          path: 'writer',
-          select: 'profile',
-        });
-      posts.push(post);
-    }
+    const posts = await getPostsByTag(tags);
     const response = posts;
     res.send(response);
   })
