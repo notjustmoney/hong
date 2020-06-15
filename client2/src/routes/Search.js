5a1a73d18f52a1847193baa9bb7587d915c79034
starpark <62285975+starpark@users.noreply.github.com>
2020-06-15 23:29:06 +0900

    seperation search component

--- client2/src/routes/Search.js
+++ client2/src/routes/Search.js
@@ -4,6 +4,8 @@ import { Icon } from "semantic-ui-react";
 import apis from "../api";
 import Cards from "../components/Cards";
 import styled from "styled-components";
+import ByTags from "./SearchByTags";
+import ByUser from "./SearchByUser";
 
 const Container = styled.div`
   width: 1250px;
@@ -57,41 +59,25 @@ const Tag = styled.div`
   }
 `;
 
-export default withRouter((props) => {
+export default withRouter(() => {
   const [posts, setPost] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [searchType, setSearchType] = useState(null);
+  const urlParams = new URLSearchParams(window.location.search);
   const uH = useHistory();
-  const {
-    match: {
-      params: { id },
-    },
-  } = props;
-  const tags = props.location.search;
-  const tagsArray = decodeURIComponent(tags.split("=")[1]).split(",");
-  
-  const searchByTag = async (id) => {
-    try {
-      const resp = await apis.searchByTag(id);
-      setPost(resp.data);
-    } catch (e) {
-      console.log(e);
-    } finally {
-      setLoading(false);
-    }
-  };
 
-  const handleRemoveTag = (index) => {
-    tagsArray.splice(index,1);
-    if(tagsArray.length === 0){
-      uH.push(`/main`);
-      return; 
-    }
-    const url = encodeURIComponent(tagsArray);
-    uH.push(`/search?tagname=${url}`);
-  }
+  
 
   useEffect(() => {
-    searchByTag(id);
+    if(urlParams.get("userId")){
+      setSearchType("userId");
+    }
+    else if(urlParams.get("tags")){
+      setSearchType("tags");
+    }
+    else{
+      setSearchType(null);
+    }
   }, []);
 
   console.log(posts);
@@ -99,42 +85,11 @@ export default withRouter((props) => {
   return (
     <>
       <Container>
-        <HashTags>
-          {tagsArray && tagsArray.map((tag, index) => 
-            <Tag key={index} onClick={() => handleRemoveTag(index)}>
-              #{tag}<Icon name="x" />
-            </Tag>
-          )}
-        </HashTags>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <>
-            {loading ? (
-              <div>Loading...</div>
-            ) : (
-              <Grid>
-                {posts && posts.map((post, index) =>
-                  post === null ? (
-                    <div key={index}></div>
-                  ) : (
-                    <Cards
-                      key={index}
-                      id={post.id}
-                      title={post.title}
-                      contents={post.contents}
-                      imgs={`http://www.hongsick.com${post.imgs[0]}`}
-                      tags={post.tags}
-                      price={post.price}
-                      writer={post.writer.profile.nickname}
-                      imgsLength={post.imgs.length}
-                    />
-                  )
-                )}
-              </Grid>
-            )}
-          </>
-        )}
+        <>
+          {searchType === "userId" && <><ByUser /></>}
+          {searchType === "tags" && <><ByTags /></>}
+          {searchType === null && <>잘못된 주소로의 접근입니다.</>}
+        </>
       </Container>
     </>
   );
